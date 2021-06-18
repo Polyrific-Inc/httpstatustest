84925343461a0cda236598b712b7acdcfd160cdc
Frandi Dwijayanto <frandi.dwijayanto@polyrific.com>
2021-06-18 10:25:27 +0700

    Init implementation of the middleware

---
+++ src/middleware/HttpStatusTestExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polyrific.Middleware.HttpStatusTest
+{
+    public static class HttpStatusTestExtensions
+    {
+        /// <summary>
+        /// Inject the HttpStatus Test middleware to the request pipeline
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseHttpStatusTest(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<HttpStatusTestMiddleware>();
+        }
+    }
+}
---
+++ src/middleware/HttpStatusTestMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Polyrific.Middleware.HttpStatusTest
+{
+    /// <summary>
+    /// Middleware to add an endpoint for the HttpStatus Test
+    /// </summary>
+    public class HttpStatusTestMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public HttpStatusTestMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path == "/httpstatus")
+            {
+                var expectedCode = context.Request.Query["c"];
+                if (!string.IsNullOrEmpty(expectedCode) && int.TryParse(expectedCode, out int code))
+                    context.Response.StatusCode = code;
+                else
+                    context.Response.StatusCode = StatusCodes.Status200OK;
+
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
